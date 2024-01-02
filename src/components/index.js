ca2225ae14b4c83670c263aed238b2a3419f8158
Leo Braga <leonathan.braga@g.globo>
2024-01-02 18:06:04 -0300

    chore: implement index

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import MovieCard from "./movie-card/MovieCard";
+import MovieList from "./movie-list/MovieList";
+import SearchInput from "./search/Search";
+import Sidebar from "./sidebar/Sidebar";
+import VideoCard from "./video-card/VideoCard";
+import VideoGallery from "./video-gallery/VideoGallery";
+
+export { MovieCard, SearchInput, Sidebar, MovieList, VideoCard, VideoGallery };
---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import MyList from "./MyList";
+import Profile from "./Profile";
+import Search from "./Search";
+import RootComponent from "./root";
+import Home from "./Home/Home"
+
+export { MyList, Profile, Search, RootComponent, Home };
